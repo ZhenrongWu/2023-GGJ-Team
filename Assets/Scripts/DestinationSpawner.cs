037065308ddd40ceff93652c7f82fe9c37a0fb6e
MSI\Frank <tim4224171@gmail.com>
2023-02-05 03:05:09 +0800

    spawn random target position

---
+++ Assets/Scripts/DestinationSpawner.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class DestinationSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform  targetTrans1, targetTrans2;
+    [SerializeField] private GameObject puddle,       mound;
+
+    private void Start()
+    {
+        int randomNumber = Random.Range(0, 2);
+        if (randomNumber == 0)
+        {
+            Instantiate(puddle, targetTrans1);
+            Instantiate(mound,  targetTrans2);
+        }
+        else
+        {
+            Instantiate(mound,  targetTrans1);
+            Instantiate(puddle, targetTrans2);
+        }
+    }
+}
